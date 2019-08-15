f1f8e736ca0ec31d6c5aafc70d96af1b7cdad3cc
a_bolt_of_lightning <46372775+a-bolt-of-lightning@users.noreply.github.com>
2019-08-15 16:14:40 +0430

    add components

---
+++ src/components/Task.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Task extends Component {
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Task;
---
+++ src/components/TaskList.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Task from './Task';
+
+class TaskList extends Component {
+
+    render(){
+        return(
+            <div>
+               <Task />  
+            </div>
+        );
+    }
+}
+
+export default TaskList;
